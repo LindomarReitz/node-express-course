afd941a03ca41b954ca6322b791e747b8e573efb
Lindomar Reitz <lindomar.reitz@gmail.com>
2019-12-07 17:33:59 +0000

    Add API tests

---
+++ test/api.test.js
@@ -0,0 +1,56 @@
+const assert = require('chai').assert;
+const request = require("supertest");
+
+const app = require('../server')
+
+describe("GET /users", function () {
+    it("returns a list of users", function (done) {
+        request(app)
+            .get("/users")
+            .expect(200, {
+                success: true,
+                message: "successfully got users. Nice!",
+                users: [
+                    {
+                        name: 'Mark'
+                    },
+                    {
+                        name: 'Jill'
+                    }
+                ]
+            }, done);
+    });
+
+    it("returns a user", function (done) {
+        request(app)
+            .get("/users/mark")
+            .expect(200, {
+                success: true,
+                message: "got one user",
+                user: "mark"
+            }, done);
+    });
+});
+
+describe("POST /login", function () {
+    it("successful login", function (done) {
+        request(app)
+            .post("/login")
+            .send({ username: "billyTheKid", password: "superSecret" })
+            .expect(200, {
+                success: true,
+                message: "password and username match!",
+                token: "encrypted token goes here"
+            }, done);
+    });
+
+    it("unsuccessful login", function (done) {
+        request(app)
+            .post("/login")
+            .send({ username: "billyTheKid", password: "invalidPassword" })
+            .expect(200, {
+                success: false,
+                message: "password and username do not match",
+            }, done);
+    });
+});
